7577cbdcf33a38ac04602f6646414bc577a21960
Gabriel Graitzer <ggraitzer@stigov.com>
2016-01-06 08:06:17 -0800

    Initial checkin, pulling system data

---
+++ EliteData.ConsoleApplication/Models/SolarSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliteData.ConsoleApplication.Models
+{
+    public class SolarSystem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+        public string faction { get; set; }
+        public ulong? population { get; set; }
+        public string government { get; set; }
+        public string allegiance { get; set; }
+        public string state { get; set; }
+        public string security { get; set; }
+        public string primary_economy { get; set; }
+        public string power { get; set; }
+        public string power_state { get; set; }
+        public int? needs_permit { get; set; }
+        public uint updated_at { get; set; }
+        public string simbad_ref { get; set; }
+    }
+}
---
+++ EliteData.ConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using EliteData.ConsoleApplication.Models;
+using Newtonsoft.Json;
+
+namespace EliteData.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Systems...");
+            var result = Task.Run(() => GetSystems()).Result;
+            Console.WriteLine("Content Received:");
+            var systems = result.Where(m => m.population != null && m.population != 0).OrderBy(m => m.population).Reverse().Take(20).ToList();
+            foreach (var s in systems)
+            {
+                Console.WriteLine($"Name: {s.Name} Population: {s.population}");
+            }
+            Console.ReadKey();
+        }
+
+        private static async Task<IEnumerable<SolarSystem>> GetSystems()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://eddb.io/archive/v4/systems.json");
+            var content = await response.Content.ReadAsStringAsync();
+            IEnumerable<SolarSystem> systemList = JsonConvert.DeserializeObject<IEnumerable<SolarSystem>>(content);
+
+            return systemList;
+        }
+    }
+}
